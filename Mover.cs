88cdae5888ac7a3cdeebcf57acd1cd84f88709f9
Ben Franklin <225748@live.brock.ac.uk>
2024-06-10 20:33:12 +0100

    Add project files.

---
+++ Mover.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using System.Data.Common;
+using System;
+using System.Drawing;
+
+namespace GraphicsProgrammingTask3_.Net_Framework_
+{
+    public class Mover
+    {
+        static Random rnd = new Random();
+        Vector2 location;
+        Vector2 velocity;
+
+        public Mover(int width, int height)
+        {
+            location = new Vector2(width/2,height/2); //locates it in the middle of the screen
+            velocity = new Vector2(3, 2);  //ratio of horizontal to verticle movement
+        }
+
+        public void Update()
+        {
+            velocity.X = rnd.Next(-5, 5);
+            velocity.Y = rnd.Next(-5, 5);
+            location = Vector2.Add(location, velocity);
+
+
+        }
+
+        public void Display(Graphics e)
+        {
+            e.FillEllipse(Brushes.BlueViolet, location.X, location.Y, 100, 100);
+        }
+
+
+    }
+
+}
---
+++ frmBBTask3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsProgrammingTask3_.Net_Framework_
+{
+    public partial class frmBBTask3 : Form
+    {
+        Mover mover; //declared outside for global access
+
+        public frmBBTask3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+            Timer timer1 = new Timer();
+            timer1.Interval = 10;
+            timer1.Enabled = true;
+            timer1.Tick += Timer1_Tick;
+            this.Paint += Form1_Paint;
+            mover = new Mover(this.Width, this.Height);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            mover.Update();
+            mover.Display(e.Graphics);
+        }
+    }
+}
